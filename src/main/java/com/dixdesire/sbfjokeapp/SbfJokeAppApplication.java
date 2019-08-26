34d80ec3f23da5c68f93cb84d1f516f1ceba780a
dixdesire <dixitwadhwani@gmail.com>
2019-08-26 17:44:21 +0530

    Initial commit

---
+++ src/main/java/com/dixdesire/sbfjokeapp/SbfJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.dixdesire.sbfjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbfJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbfJokeAppApplication.class, args);
+    }
+
+}
